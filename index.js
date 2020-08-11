c8f035cfbaace55f9606e3bf6f2f31d1553d5e46
remarkable-assembler-5835 <mdrew8@gmail.com>
2020-08-11 01:25:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function add(a, b) {a + b}
 function subtract(a, b) {b - a}
-function multiply() {number *= 4}
-function divide() {number /= 2}
+function multiply(a, b) {a * b}
+function divide(a, b) {a / b}
 function 

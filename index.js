d584234568007be3e784ee3670aa723a6791a9d8
remarkable-assembler-5835 <mdrew8@gmail.com>
2020-08-11 01:35:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ function multiply(a, b) {return a * b}
 function divide(a, b) {return a / b}
 function increment(n) {return ++n }
 function decrement(n) {return --n }
-function makeInt(n) parseInt(n, 10)
+function makeInt(n) {parseInt(n, 10)}

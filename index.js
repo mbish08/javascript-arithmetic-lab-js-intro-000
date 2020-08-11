24b54cba6f4f81b0d29eb5b4d43940561acc0fc0
remarkable-assembler-5835 <mdrew8@gmail.com>
2020-08-11 01:32:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function add(a, b) {return a + b}
 function subtract(a, b) {return a - b}
 function multiply(a, b) {return a * b}
 function divide(a, b) {return a / b}
-function increment(n) {return (n++) }
+function increment(n) {return ++n }
 function decrement(n) {return n-- }
